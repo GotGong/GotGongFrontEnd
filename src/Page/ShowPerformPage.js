a04109cc2d1b23aa708d087419ef1d39cc09fd61
DoYam <doniang1123@gmail.com>
2022-08-03 20:05:52 +0900

    #13 [FEAT] makeroompage api통신 확인

---
+++ src/Page/ShowPerformPage.js
@@ -0,0 +1,15 @@
+import React , {useState} from 'react';
+
+
+function ShowPlanPage(token){
+
+
+    return(
+        <div>
+            <h3>수행 조회 페이지입니다.</h3>
+        </div>
+    )
+
+};
+
+export default ShowPlanPage;
