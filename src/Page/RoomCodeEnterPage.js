c6c39a08055bf0824fe4ebbb62eb8848369d5eb7
ch9eri <limch9eri@gmail.com>
2022-08-03 18:18:49 +0900

    [FIX]  #11 roomEnterAPI 수정

--- src/Page/RoomCodeEnterPage.js
+++ src/Page/RoomCodeEnterPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+//import axios from "axios";
 import roomCodeEnterAPI from "../API/roomCodeEnterAPI";
 
 function RoomCodeEnterPage({ token, setToken }) {
@@ -24,13 +25,44 @@ function RoomCodeEnterPage({ token, setToken }) {
       })
   }
 
+//   const codeEnter = ((room_code,token) =>  {
+//     let room_id = ''
+//     axios.post("http://localhost:8000/room/enterbycode/",  {
+//         room_code: room_code,
+//     },{
+//         headers: {
+//             Authorization: `Token ${token}`
+//         }
+//     })
+//     .then((response)=> {
+//       console.log("roomcode인식성공");
+//         if (response !== ''){
+//             console.log(response);
+//             room_id = response;
+//             setRoom_id(response);
+//             setRoom_code('');
+//             navigate('/myrooms');
+//         }
+
+//         else{
+//             console.log(response);
+//             alert('해당 코드가 존재하지 않습니다.');
+//         }
+//     })
+//     .catch(function(error){
+//       console.log(error);
+//       console.log("roomcode인식실패");
+//   });
+// });
+
   return (
     <div className="RoomCodeEnterPageContainer">
+      {token}
       <div>
         <h1>RoomCodeEnterPage입니다.</h1>
       </div>
       <div>
-        <input name="roomcode" type="text" placeholder="참여코드" />
+        <input name="roomcode" type="text" placeholder="참여코드" value={room_code} onChange={(e) => setRoom_code(e.target.value)} />
         <button onClick={codeEnter}>참여하기</button>
       </div>
     </div>
